2e81a565c58fc6259cb04d255eabc7d255a11117
Groonware <groonware@gmail.com>
2019-11-22 15:26:31 +0800

    Update Pencarian, Subjek Data

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware('ragam:api')->group(function(){
     Route::get('/indikator/terakhir', 'IndikatorAPIController@indikatorTerakhir');
+    Route::get('/indikator/subjek/{idSubjek}','IndikatorAPIController@tampilkanBySubjek');
     Route::get('/indikator/{idIndikator}', 'IndikatorAPIController@tampilkan');
-    Route::get('/cari', 'IndikatorAPIController@cari');;
+    Route::get('/cari', 'IndikatorAPIController@cari');
+    Route::get('/subjek/semua', 'IndikatorAPIController@semuaSubjek');
+    Route::get('/metadata/{idMetadata}', 'IndikatorAPIController@lihatMetadata');
  });
 
 Route::get('getDataApi', function() {
