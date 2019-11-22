0eb79eb8f12547a775d291bc62d23d35ad690aff
Groonware <groonware@gmail.com>
2019-11-22 15:42:36 +0800

    Dekor Subjek

---
+++ database/seeds/msubjek.sql
@@ -0,0 +1,10 @@
+INSERT INTO `msubjek` (`id`, `nama_subjek`) VALUES
+(1,	'Administrasi Pemerintahan'),
+(2,	'Penduduk & Tenaga Kerja'),
+(3,	'Pendidikan'),
+(4,	'Kesehatan'),
+(5,	'Sosial Masyarakat'),
+(6,	'Pertanian'),
+(7,	'Industri & Perdagangan'),
+(8,	'Pariwisata & Transportasi'),
+(9,	'Keuangan & Ekonomi');
